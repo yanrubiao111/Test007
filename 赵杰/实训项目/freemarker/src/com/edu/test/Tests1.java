161103aff8cbf5d2b42ede18634ea47054d42dc4
zhaojiezj <33645026+zhaojiezj@users.noreply.github.com>
2019-12-24 13:48:13 +0800

    Add files via upload

---
+++ 赵杰/实训项目/freemarker/src/com/edu/test/Tests1.java
@@ -0,0 +1,23 @@
+package com.edu.test;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import com.webtest.core.BaseTest;
+
+
+public class Tests1{
+	@Test
+	public void test01(){
+		System.out.println("大家好");
+	}
+	@Test
+	public void test02(){
+		System.out.println("菜鸟来了");
+	}
+	@Test
+	public void test03(){
+		System.out.println("多多捧场！");
+	}
+}
